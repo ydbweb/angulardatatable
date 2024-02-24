0b131e086b005153e889dd4f501f7ba3c0e542d1
ydbweb <ydbweb.com@gmail.com>
2024-02-24 17:15:47 +0100

    listing done

--- src/app/listing/listing.component.ts
+++ src/app/listing/listing.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RestService } from '../rest.service';
+import { Observable } from 'rxjs'
 
 @Component({
   selector: 'app-listing',
@@ -6,5 +8,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./listing.component.css']
 })
 export class ListingComponent {
+
+  p: Number = 1;
+  count: Number = 4;
+  items:any = [];
+  subitems:any = [];
+  isLoading=true;
+  idpost: Number = 0;
+
+  constructor(private restService : RestService) {
+    this.init1();
+  }
+  ngOnInit() {
+   
+  }
+
+
+  init1() {
+    this.restService.get_items().subscribe((res : any)=>{
+      this.items=res;
+      console.log(this.items);
+    });
+  }
+
+  show_comments(id_post : number){
+    this.idpost=id_post;
+    this.restService.get_items_comments(id_post).subscribe((subitems : any)=>{
+      this.subitems=subitems;
+      console.log(this.subitems);
+    });  
+  }
 
 }
--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RestService {
 
-  constructor() { }
+  constructor(private httpClient : HttpClient) {}
+
+  baseUrl:string = "http://localhost:8080";
+
+  get_items(){
+      return this.httpClient.get(this.baseUrl + '/listing');
+  } 
+  
+  get_items_comments(id_post : any){
+    return this.httpClient.get(this.baseUrl + '/listing_comments?id_post='+id_post);
+  }
 }
